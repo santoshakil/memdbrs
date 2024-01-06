49177fd80e365ebbc086d5f064f0cf9239c510ab
Santo Shakil <shakil.mezbah@gmail.com>
2024-01-06 16:28:58 +0600

    WIP

--- src/open.rs
+++ src/open.rs
@@ -1,3 +1,5 @@
+use std::os::fd::AsRawFd;
+
 use anyhow::Result;
 
 pub fn open(path: String) -> Result<()> {
@@ -8,8 +10,28 @@ pub fn open(path: String) -> Result<()> {
         .append(true)
         .open(path)?;
 
+    file.set_len(1024 * 1024 * 1024)?;
     let len = file.metadata()?.len();
-    log::info!("File size: {}", len);
+    log::info!("File size: {}mb", len / 1024 / 1024);
+
+    let memory = unsafe {
+        libc::mmap(
+            std::ptr::null_mut(),
+            len as usize,
+            libc::PROT_READ | libc::PROT_WRITE,
+            libc::MAP_SHARED,
+            file.as_raw_fd(),
+            0,
+        )
+    };
+
+    if memory == libc::MAP_FAILED {
+        anyhow::bail!("Failed to mmap file");
+    }
+
+    log::info!("Start address: {:p}", memory);
+    let end_address = (memory as usize) + len as usize;
+    log::info!("End address: {:p}", end_address as *const u8);
 
     Ok(())
 }
@@ -21,6 +43,9 @@ mod tests {
     #[test]
     fn test_open() {
         crate::init::init_log().unwrap();
-        assert!(open("./default.memdbrs".to_owned()).is_ok());
+        match open("./default.memdbrs".to_owned()) {
+            Ok(_) => (),
+            Err(e) => panic!("Open failed with error: {}", e),
+        }
     }
 }
